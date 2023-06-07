3e184ca05fa22d726b592421adf543110d219ef3
ArumDwiCahyani <134916757+ArumDwiCahyani@users.noreply.github.com>
2023-06-07 21:27:10 +0700

    Add files via upload

---
+++ latihan-springboot/latihan-springboot/src/main/java/com/kuliah/latihanspringboot/model/AppUser.java
@@ -0,0 +1,22 @@
+package com.kuliah.latihanspringboot.model;
+
+import lombok.*;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AppUser {
+    private UUID id_user;
+
+    //GENERAL INFORMATION
+    private String fname;
+    private String lname;
+    private String email;
+
+    //AUTHENTICATION INFORMATIONS
+    private String username;
+    private String password;
+}
